71489eacf9c61b94dbf2389d23eb8c6819592b31
sivilll <kje.sigurdsson@gmail.com>
2024-11-01 22:43:08 +0100

    Comments on javascript file (for hamburger navigation) and making it slightly transparent

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* I don't remember where I found this functionality - I didn't write it myself */
+
 // Hamburger menu
 const hamburger = document.getElementById("hamburger");
 const nav = document.getElementById("nav");
